c1cb4e2572b86d8c8fc5049cad6d0037698595a8
jackie <donghs@outlook.com>
2022-12-07 14:23:53 +0800

    add message channel receiver

--- src/main/java/com/gt/demo/notification/adapter/inbound/NotificationConsumer.java
+++ src/main/java/com/gt/demo/notification/adapter/inbound/NotificationConsumer.java
@@ -1,8 +1,10 @@
 package com.gt.demo.notification.adapter.inbound;
 import com.gt.demo.notification.dto.NotificationRequest;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.integration.channel.QueueChannel;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
@@ -15,7 +17,10 @@ import static com.gt.demo.notification.config.ActiveMQConfig.NTF_QUEUE;
 
 @Component
 @Slf4j
+@AllArgsConstructor
 public class NotificationConsumer {
+
+    private final QueueChannel queueChannel;
     @JmsListener(destination = NTF_QUEUE)
     public void receiveMessage(@Payload NotificationRequest request,
                                @Headers MessageHeaders headers,
@@ -29,5 +34,10 @@ public class NotificationConsumer {
         log.info("message: " + message.getPayload());
         log.info("session: " + session);
         log.info("- - - - - - - - - - - - - - - - - - - - - - - -");
+
+
+        log.info(" Send message to Queue Channel");
+        queueChannel.send(message);
+        log.info(" Queue Size:" + queueChannel.getQueueSize());
     }
 }
---
+++ src/main/java/com/gt/demo/notification/service/NotificationQueueService.java
@@ -0,0 +1,22 @@
+package com.gt.demo.notification.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.messaging.Message;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class NotificationQueueService {
+    private final QueueChannel queueChannel;
+
+    @Scheduled(fixedDelay = 1000)
+    public void distributeNotification(){
+        log.info("$$$$$$$Start distribute message");
+        Message message = queueChannel.receive();
+        log.info("$$$$$$$message {} distributed", message.getPayload());
+    }
+}
