4c7b185deef7a3365fe41315ac54c73625e1e8dc
jackie <donghs@outlook.com>
2022-12-08 15:46:49 +0800

    Update NotificationApplication.java

--- src/main/java/com/gt/demo/notification/NotificationApplication.java
+++ src/main/java/com/gt/demo/notification/NotificationApplication.java
@@ -1,20 +1,14 @@
 package com.gt.demo.notification;
 
-import com.gt.demo.notification.service.NotificationQueueService;
-import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.integration.channel.QueueChannel;
-import org.springframework.messaging.Message;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.Queue;
 
 @SpringBootApplication
 @EnableScheduling
 public class NotificationApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(NotificationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationApplication.class, args);
+    }
 
 }
